d7f5b141072a2c3f9e070e0ad48bb87f1b810922
“brianbaso” <brianbaso@gmail.com>
2022-09-30 18:45:11 -0400

    attempt 5

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import '../index.css'
 import AnchorLink from "react-anchor-link-smooth-scroll";
 import {FaHamburger} from 'react-icons/fa'
 import {useState} from "react";
-// import { isMobile } from "react-device-detect";
+import { isMobile } from "react-device-detect";
 
 
 export const Navbar = () => {
@@ -16,13 +16,15 @@ export const Navbar = () => {
                     <FaHamburger fontSize="1.5rem"/>
                 </button>
             </div>
-            <div className={window.innerWidth > 600 ? "nav-flex-container" : (isOpen ? "nav-flex-container" : "nav-flex-container-hide")}>
+            <div className={isMobile ? (isOpen ? "nav-flex-container" : "nav-flex-container-hide") : "nav-flex-container"}>
                 <div className="nav-item"><AnchorLink href='#home'>Home</AnchorLink></div>
                 <div className="nav-item"><AnchorLink href='#skills'>Skills</AnchorLink></div>
                 <div className="nav-item"><AnchorLink href='#education'>Education</AnchorLink></div>
                 <div className="nav-item"><AnchorLink href='#experience'>Experience</AnchorLink></div>
                 <div className="nav-item"><AnchorLink href='#projects'>Projects</AnchorLink></div>
                 <div className="nav-item"><AnchorLink href='#interests'>Interests</AnchorLink></div>
+                {/* <div className="nav-item">isOpen: {String(isOpen)}</div>
+                <div className="nav-item">isOpen: {String(isOpen)}</div> */}
             </div>
         </>
     );
