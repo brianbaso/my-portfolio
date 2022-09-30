c54713c9bdaa3b56dd6c2c599169be29d09a67aa
“brianbaso” <brianbaso@gmail.com>
2022-09-30 18:24:01 -0400

    trying something different for this bug

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,13 +3,11 @@ import '../index.css'
 import AnchorLink from "react-anchor-link-smooth-scroll";
 import {FaHamburger} from 'react-icons/fa'
 import {useState} from "react";
-import { isMobile } from "react-device-detect";
+// import { isMobile } from "react-device-detect";
 
 
 export const Navbar = () => {
     const [isOpen, setIsOpen] = useState(false)
-
-    console.log(isMobile, isOpen)
 
     return (
         <>
@@ -18,15 +16,13 @@ export const Navbar = () => {
                     <FaHamburger fontSize="1.5rem"/>
                 </button>
             </div>
-            <div className={isMobile ? (isOpen ? "nav-flex-container" : "nav-flex-container-hide") : "nav-flex-container"}>
+            <div className={window.innerWidth > 600 ? "nav-flex-container" : (isOpen ? "nav-flex-container" : "nav-flex-container-hide")}>
                 <div className="nav-item"><AnchorLink href='#home'>Home</AnchorLink></div>
                 <div className="nav-item"><AnchorLink href='#skills'>Skills</AnchorLink></div>
                 <div className="nav-item"><AnchorLink href='#education'>Education</AnchorLink></div>
                 <div className="nav-item"><AnchorLink href='#experience'>Experience</AnchorLink></div>
                 <div className="nav-item"><AnchorLink href='#projects'>Projects</AnchorLink></div>
                 <div className="nav-item"><AnchorLink href='#interests'>Interests</AnchorLink></div>
-                <div className="nav-item">isOpen: {String(isOpen)}</div>
-                <div className="nav-item">isMobile: {String(isMobile)}</div>
             </div>
         </>
     );
